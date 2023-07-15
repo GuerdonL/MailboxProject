31fab5e7ea133c517d5387d9f04cc70370adc0e4
GuerdonL <guerdon@ligon.org>
2023-07-14 22:48:42 -0400

    map completed :)

---
+++ public/app.js
@@ -0,0 +1,123 @@
+const mapStyle = [{
+  'featureType': 'administrative',
+  'elementType': 'all',
+  'stylers': [{
+    'visibility': 'on',
+  },
+  {
+    'lightness': 33,
+  },
+  ],
+},
+{
+  'featureType': 'landscape',
+  'elementType': 'all',
+  'stylers': [{
+    'color': '#f2e5d4',
+  }],
+},
+{
+  'featureType': 'poi.park',
+  'elementType': 'geometry',
+  'stylers': [{
+    'color': '#c5dac6',
+  }],
+},
+{
+  'featureType': 'poi.park',
+  'elementType': 'labels',
+  'stylers': [{
+    'visibility': 'on',
+  },
+  {
+    'lightness': 20,
+  },
+  ],
+},
+{
+  'featureType': 'road',
+  'elementType': 'all',
+  'stylers': [{
+    'lightness': 20,
+  }],
+},
+{
+  'featureType': 'road.highway',
+  'elementType': 'geometry',
+  'stylers': [{
+    'color': '#c5c6c6',
+  }],
+},
+{
+  'featureType': 'road.arterial',
+  'elementType': 'geometry',
+  'stylers': [{
+    'color': '#e4d7c6',
+  }],
+},
+{
+  'featureType': 'road.local',
+  'elementType': 'geometry',
+  'stylers': [{
+    'color': '#fbfaf7',
+  }],
+},
+{
+  'featureType': 'water',
+  'elementType': 'all',
+  'stylers': [{
+    'visibility': 'on',
+  },
+  {
+    'color': '#acbcc9',
+  },
+  ],
+},
+];
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4.61,
+        center: {lat: 38.2284531, lng: -95.166094},
+        styles: mapStyle
+    });
+
+    map.data.loadGeoJson('file.json', {idPropertyName: 'store_id'});
+
+    map.data.setStyle((feature) => {
+        return {
+          icon: {
+            url: `mail_icon.svg`,
+            scaledSize: new google.maps.Size(32, 32),
+          },
+        };
+      });
+
+    const apiKey = 'AIzaSyDcU7vPIAONsO5GZnOnip7SOI4ElZcGKcE';
+    const infoWindow = new google.maps.InfoWindow();
+
+    // Show the information for a store when its marker is clicked.
+    map.data.addListener('click', (event) => {
+      const name = event.feature.getProperty('name');
+      const url = event.feature.getProperty('url');
+      const address = event.feature.getProperty('address');
+      const position = event.feature.getGeometry().get();
+      const content = `
+      <img style="float:left; width:80px; margin-top:30px" src="mail_icon.svg">
+      <div style="margin-left:100px; margin-bottom:20px; margin-top: 20px">
+        <h2>${name}</h2>
+        <br/>
+        <p>
+        <b>Details:</b> 
+        <br/>
+        <a href=${url} >${url}</a><br/><b>Address:</b> ${address}</p>
+        <p><img src="https://maps.googleapis.com/maps/api/streetview?size=350x120&location=${position.lat()},${position.lng()}&key=${apiKey}&solution_channel=GMP_codelabs_simplestorelocator_v1_a"></p>
+      </div>
+      `;
+  
+      infoWindow.setContent(content);
+      infoWindow.setPosition(position);
+      infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+      infoWindow.open(map);
+    });
+}
--- scraping/thing3.py
+++ scraping/thing3.py
@@ -2,26 +2,49 @@ import csv,json
 from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="myGeocoder")
-# open file2.csv
-with open('file2.csv', 'r') as f:
+ 
+# open file2.csv, a file with store data pulled from the url list in file.csv
+with open("/home/me/mp_test/scraping/file2.csv", 'r') as f:
     reader = csv.reader(f)
     header = next(reader)
+    
+    # create a data object that holds each store object of data as a seperate list in a list of the form [placename, url, address]
     data = [row for row in reader]
+
+    #iterate through and clean each row
     for j,row in enumerate(data):
         for i,elem in enumerate(row):
             row[i] = elem.replace('\n', ' ').replace("\u2122", ' ').replace("\u2014", ' ').replace(" STATE "," ").replace(" HIGHWAY ", " ").replace(" N "," ").replace(" S "," ").replace(" E "," ").replace(" W "," ").replace("    ", ' ').replace("   ", ' ').replace("  ", ' ').strip()
+    
+    #iterate through and geocode each row, geocoding the address, and replacing the address with the geocoded address, and appending the lat/long to the end of the store object
+    for j,row in enumerate(data):
         try:
+            # geocode the address
             location = geolocator.geocode(row[2])
+            # replace the address with the geocoded address
             row[2] = location.address
+            # append the lat/long to the end of the store object
             row.append((location.latitude, location.longitude))
-            print(location.address)
+            # print the progress
+            print(str(j) + " of " + str(len(data)) + " geocoded")
         except:
             try:
                 print("errored: " + row[2])
                 del data[j]
             except:
                 del data[j]
+                print("double error")
+                continue
+    # we will format the data into a list of dictionaries, where each dictionary is a store object of the form: {"geometry": {"type": "Point", "coordinates": [LAT,LONG]},"type": "Feature","properties": {"name": NAME,"url": URL,"address": ADDRESS}}
+    # iterate through the data and create a dictionary for each store object
+    for j,row in enumerate(data):
+        try:
+            data[j] = {"geometry": {"type": "Point", "coordinates": [row[3][1],row[3][0]]},"type": "Feature","properties": {"name": row[0],"url": row[1],"address": row[2],"store_id": j}}
+        except:
+            print(data[j])
+    # finally we place the data into a dictionary of the form: {"type": "FeatureCollection","features": [STORE1,STORE2,STORE3,...]}
+    data = {"type": "FeatureCollection","features": data}
 
-# 199 5TH ST W CHAMA, NM 87520-9998
-with open('file.json', 'w') as f:
+with open('/home/me/mp_test/scraping/file.json', 'w') as f:
     json.dump(data, f)
+    print("done")
